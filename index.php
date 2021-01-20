82dc82b484cc68a2a4cd0a1255f5ff5c106940f2
Беккель Маргарита Владимировна <@corp.itstep>
2021-01-20 20:00:55 +0600

    update

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +11,8 @@
 </head>
 <body>
     <div class="main">
-
+<div class="b"></div>
+<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste porro facere debitis ratione quia corrupti vero veritatis commodi autem consequatur modi, labore fuga quidem incidunt eaque nemo blanditiis eligendi aperiam!</h1>
         
     </div>
 </body>
